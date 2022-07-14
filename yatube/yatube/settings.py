5d5375ba3770d8957edc671250fb5a2dcdf9aea4
BnamoRS <rbutskih@mail.ru>
2022-07-14 22:01:45 +0300

    убрал debug-toolbar

--- yatube/yatube/settings.py
+++ yatube/yatube/settings.py
@@ -52,7 +52,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "posts.apps.PostsConfig",
     "sorl.thumbnail",
-    'debug_toolbar',
+    # 'debug_toolbar',
 ]
 
 MIDDLEWARE = [
@@ -63,7 +63,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
-    'debug_toolbar.middleware.DebugToolbarMiddleware',
+    # 'debug_toolbar.middleware.DebugToolbarMiddleware',
 ]
 
 
